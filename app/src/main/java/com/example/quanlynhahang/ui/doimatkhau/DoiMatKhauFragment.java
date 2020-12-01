65bffbf2594d603d5d0fc4147fad0abb91379b8f
nmtoan <nmtoan.cmu@gmail.com>
2020-12-01 10:14:14 +0700

    create layout (tt)

--- app/src/main/java/com/example/quanlynhahang/ui/doimatkhau/DoiMatKhauFragment.java
+++ app/src/main/java/com/example/quanlynhahang/ui/doimatkhau/DoiMatKhauFragment.java
@@ -23,13 +23,13 @@ public class DoiMatKhauFragment extends Fragment {
         doiMatKhauViewModel =
                 new ViewModelProvider(this).get(DoiMatKhauViewModel.class);
         View root = inflater.inflate(R.layout.fragment_doimatkhau, container, false);
-        final TextView textView = root.findViewById(R.id.text_doimatkhau);
-        doiMatKhauViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_doimatkhau);
+//        doiMatKhauViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
--- app/src/main/java/com/example/quanlynhahang/ui/thongtincanhan/ThongTinCaNhanFragment.java
+++ app/src/main/java/com/example/quanlynhahang/ui/thongtincanhan/ThongTinCaNhanFragment.java
@@ -23,13 +23,13 @@ public class ThongTinCaNhanFragment extends Fragment {
         thongTinCaNhanViewModel =
                 new ViewModelProvider(this).get(ThongTinCaNhanViewModel.class);
         View root = inflater.inflate(R.layout.fragment_thongtincanhan, container, false);
-        final TextView textView = root.findViewById(R.id.text_thongtincanhan);
-        thongTinCaNhanViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_thongtincanhan);
+//        thongTinCaNhanViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
